ee3f354ad7e39c8c583c2caf9261864af1010b60
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-24 18:53:20 +0300

    add ValidationMessages.cs

---
+++ src/Weather/Weather.Web/Services/Validation/ValidationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Weather.Web.Services.Validation;
+
+public class ValidationMessages
+{
+    public const string InvalidFileExtension = "Некорректное расширение файла";
+    public const string UnableToOpenFile = "Невозможно открыть файл";
+    public const string UnableToMapFile = "Невозможно сопоставить данные таблиц";
+    public const string UnableToReadFile = "Невозможно прочесть данные";
+}
