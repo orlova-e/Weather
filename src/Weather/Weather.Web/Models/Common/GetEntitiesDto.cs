4182086c28b3d3cd38af68c99dff37262dcb6a94
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-24 18:50:22 +0300

    add common models

---
+++ src/Weather/Weather.Web/Models/Common/GetEntitiesDto.cs
@@ -0,0 +1,10 @@
+﻿using Weather.Infrastructure.Conditions;
+
+namespace Weather.Web.Models.Common;
+
+public class GetEntitiesDto
+{
+    public int Page { get; set; } = 1;
+    public int ItemsNumber { get; set; } = 20;
+    public SortBy SortBy { get; set; } = SortBy.Desc;
+}
---
+++ src/Weather/Weather.Web/Models/Common/ListDto.cs
@@ -0,0 +1,13 @@
+﻿using Weather.Infrastructure.Conditions;
+
+namespace Weather.Web.Models.Common;
+
+public class ListDto<T>
+{
+    public int CurrentPage { get; set; }
+    public int ItemsPerPage { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages { get; set; }
+    public SortBy ListSorting { get; set; }
+    public IEnumerable<T> Entities { get; set; }
+}
