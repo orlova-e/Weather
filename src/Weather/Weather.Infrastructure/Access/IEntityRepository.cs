c110f969aba7acf1382ebb60f237498dfab56798
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-24 18:33:07 +0300

    add repository interface and its implementation

---
+++ src/Weather/Weather.Infrastructure/Access/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Weather.Domain.Entities;
+
+namespace Weather.Infrastructure.Access;
+
+public interface IEntityRepository<T>
+    where T : class, IEntity
+{
+    Task<T> GetAsync(Guid id);
+    Task<int> CountAsync();
+    Task<List<T>> ListAsync();
+    Task<List<T>> ListAsync(int page, int count, string sortField, bool asc = true);
+    Task CreateAsync(T entity);
+    Task CreateAsync(IEnumerable<T> entities);
+}
---
+++ src/Weather/Weather.Infrastructure/Access/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Weather.Domain.Entities;
+using Weather.Infrastructure.Mappings;
+
+namespace Weather.Infrastructure.Access;
+
+public class Repository<T> : IEntityRepository<T>
+    where T : class, IEntity
+{
+    private readonly Context _context;
+
+    public Repository(Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> GetAsync(Guid id)
+    {
+        return await _context.FindAsync<T>(id);
+    }
+
+    public Task<int> CountAsync()
+    {
+        return _context.Set<T>().CountAsync();
+    }
+
+    public Task<List<T>> ListAsync()
+    {
+        return _context.Set<T>().ToListAsync();
+    }
+
+    public Task<List<T>> ListAsync(
+        int page,
+        int count,
+        string sortField,
+        bool asc = true)
+    {
+        return _context
+            .Set<T>()
+            .Skip(page)
+            .Take(count)
+            .ToListAsync();
+    }
+
+    public Task CreateAsync(T entity)
+    {
+        _context.Add(entity);
+        return _context.SaveChangesAsync();
+    }
+
+    public Task CreateAsync(IEnumerable<T> entities)
+    {
+        _context.AddRange(entities);
+        return _context.SaveChangesAsync();
+    }
+}
