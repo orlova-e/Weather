86f41336c6aa283d270911b6337c3a523f2104b4
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-24 18:35:11 +0300

    add InfrastructureExtensions.cs

---
+++ src/Weather/Weather.Infrastructure/Configuration/InfrastructureExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Weather.Infrastructure.Access;
+using Weather.Infrastructure.Mappings;
+
+namespace Weather.Infrastructure.Configuration;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        
+        services
+            .AddDbContext<Context>(option => option.UseNpgsql(connectionString))
+            .AddScoped(typeof(IEntityRepository<>), typeof(Repository<>));
+
+        return services;
+    }
+}
