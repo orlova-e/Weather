3e4acdcff200f70934fba5ed3cd5fa52ad8522a5
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-24 18:53:53 +0300

    add LoadWeatherDtoValidator.cs

---
+++ src/Weather/Weather.Web/Services/Validation/Weather/LoadWeatherDtoValidator.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using Weather.Services.Files;
+using Weather.Services.MappedModels;
+using Weather.Services.Models;
+using Weather.Web.Models.Configuration;
+using Weather.Web.Models.Weather;
+
+namespace Weather.Web.Services.Validation.Weather;
+
+public class LoadWeatherDtoValidator : AbstractValidator<LoadWeatherDto>
+{
+    private readonly IOptions<ValidationOptions> _options;
+    private readonly IFileService<WeatherMapModel> _fileService;
+
+    public LoadWeatherDtoValidator(
+        IOptions<ValidationOptions> options,
+        IFileService<WeatherMapModel> fileService)
+    {
+        _options = options;
+        _fileService = fileService;
+
+        RuleForEach(x => x.Files)
+            .NotEmpty()
+            .Must(IsFileExtensionValid)
+            .WithMessage(ValidationMessages.InvalidFileExtension)
+            .CustomAsync(CanBeWritten);
+    }
+
+    private bool IsFileExtensionValid(IFormFile file)
+    {
+        var extension = Path.GetExtension(file.FileName);
+        return _options.Value.File.Extensions.Contains(extension);
+    }
+
+    private async Task CanBeWritten(
+        IFormFile file,
+        ValidationContext<LoadWeatherDto> context,
+        CancellationToken cancellationToken)
+    {
+        await using var stream = file.OpenReadStream();
+        var fileStream = stream as FileStream;
+
+        var error =  _fileService.Validate(fileStream);
+        
+        if(error is FileReadMessages.Normal)
+            return;
+
+        var failureMessage = error switch
+        {
+            FileReadMessages.UnableToMap => ValidationMessages.UnableToMapFile,
+            FileReadMessages.UnableToOpen => ValidationMessages.UnableToOpenFile,
+            FileReadMessages.UnableToRead => ValidationMessages.UnableToReadFile,
+            { } => string.Empty
+        };
+
+        context.AddFailure(failureMessage);
+    }
+}
