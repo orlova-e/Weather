e4b945995a0d694a4b1288a00e732f1077118554
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-24 18:45:55 +0300

    add ColumnMapAttribute.cs

---
+++ src/Weather/Weather.Services/MappingModels/ColumnMapAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Weather.Services.MappingModels;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ColumnMapAttribute : Attribute
+{
+    public int Index { get; }
+    public string Column { get; }
+
+    public ColumnMapAttribute(int index, string column)
+    {
+        if (index < 1)
+            throw new ArgumentException("Index must be a positive value", nameof(index));
+        
+        Index = index;
+        Column = column;
+    }
+}
