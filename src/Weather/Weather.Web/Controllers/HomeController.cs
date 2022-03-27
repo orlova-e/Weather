2af08e6fa0ef46bb18f6e447467486991126a2be
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-27 13:12:34 +0300

    add HomeController and view of Index

---
+++ src/Weather/Weather.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.Web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
