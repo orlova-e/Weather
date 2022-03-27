5ce850aaac07a037181c7837fb577ae00f2c580d
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-27 19:52:34 +0300

    add MapModelInfo.cs

---
+++ src/Weather/Weather.Services/MappingModels/MapModelInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Weather.Services.MappingModels;
+
+public class MapModelInfo<T>
+    where T : IMapModel
+{
+    public T Model { get; set; }
+    public List<PropertyInfo> PropertyInfos { get; set; }
+}
