c95cf045228d51cf13363157fa4355f0dcef078e
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-27 13:14:58 +0300

    add WebExtensions

---
+++ src/Weather/Weather.Web/Services/Configuration/WebExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+using Weather.Web.Services.Commands.Weather;
+using Weather.Web.Services.Validation.Weather;
+
+namespace Weather.Web.Services.Configuration;
+
+public static class WebExtensions
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services
+            .AddValidatorsFromAssemblyContaining<LoadWeatherDtoValidator>()
+            .AddAutoMapper(typeof(Program).Assembly)
+            .AddMediatR(typeof(GetWeatherConditionsRequest).Assembly);
+        
+        return services;
+    }
+}
