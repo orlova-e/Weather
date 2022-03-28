950496a6c5c36f2023482f757e48c435327f7517
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-28 13:20:25 +0300

    fix GetWeatherConditionsRequest and its command

--- src/Weather/Weather.Web/Services/Commands/Weather/GetWeatherConditionsCommand.cs
+++ src/Weather/Weather.Web/Services/Commands/Weather/GetWeatherConditionsCommand.cs
@@ -1,66 +1,96 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Weather.Domain.Entities;
 using Weather.Infrastructure.Access;
-using Weather.Infrastructure.Conditions;
+using Weather.Infrastructure.Helpers;
+using Weather.Services.Data;
+using Weather.Services.Files;
 using Weather.Web.Models.Common;
 using Weather.Web.Models.Weather;
 
 namespace Weather.Web.Services.Commands.Weather;
 
-public class GetWeatherConditionsCommand : IRequestHandler<GetWeatherConditionsRequest, CommandResult<ListDto<WeatherViewDto>>>
+public class GetWeatherConditionsCommand : IRequestHandler<GetWeatherConditionsRequest, CommandResult<WeatherListDto>>
 {
     private readonly IRepository _repository;
+    private readonly IInfoService _infoService;
     private readonly IMapper _mapper;
     private readonly ILogger<GetWeatherConditionsCommand> _logger;
 
     public GetWeatherConditionsCommand(
         IRepository repository,
+        IInfoService infoService,
         IMapper mapper,
         ILogger<GetWeatherConditionsCommand> logger)
     {
         _repository = repository;
+        _infoService = infoService;
         _mapper = mapper;
         _logger = logger;
     }
 
-    public async Task<CommandResult<ListDto<WeatherViewDto>>> Handle(
-        GetWeatherConditionsRequest conditionsRequest,
+    public async Task<CommandResult<WeatherListDto>> Handle(
+        GetWeatherConditionsRequest request,
         CancellationToken cancellationToken)
     {
         _logger.LogInformation("Get weather conditions command is started");
 
         try
         {
-            var entities = await _repository.WeatherConditions.ListAsync(
-                page: conditionsRequest.Dto.Page,
-                count: conditionsRequest.Dto.ItemsNumber,
-                sortField: nameof(WeatherCondition.DateTime),
-                asc: conditionsRequest.Dto.SortDir == SortDir.Asc);
-            
-            var totalEntitiesCount = await _repository.WeatherConditions.CountAsync();
-
-            var viewModels = _mapper.Map<IEnumerable<WeatherCondition>, IEnumerable<WeatherViewDto>>(entities);
-
-            var listDto = new ListDto<WeatherViewDto>()
+            Expression<Func<WeatherCondition, bool>> expression = null;
+            if (FilterParser.TryParseFilter(request.Dto.Filters, out Dictionary<string, string> filters))
             {
-                CurrentPage = conditionsRequest.Dto.Page,
-                ItemsPerPage = conditionsRequest.Dto.ItemsNumber,
-                ListSorting = conditionsRequest.Dto.SortDir,
-                TotalPages = (int) Math.Ceiling((double) totalEntitiesCount / conditionsRequest.Dto.ItemsNumber),
+                for (int i = 0; i < filters.Count; i++)
+                {
+                    var right = ExpressionHelper.BuildExpression<WeatherCondition>(
+                        filters.ElementAt(i).Key,
+                        filters.ElementAt(i).Value);
+                    
+                    if (i == 0)
+                    {
+                        expression = right;
+                        continue;
+                    }
+
+                    expression = expression.And(right);
+                }
+            }
+            
+            
+            var entities = await _repository.WeatherConditions.List(
+                wherePredicate: expression,
+                orderByKey: request.Dto.OrderBy ?? nameof(WeatherCondition.DateTime),
+                sortDir: request.Dto.SortDir,
+                skip: request.Dto.Page * request.Dto.ItemsNumber,
+                take: request.Dto.ItemsNumber);
+            
+            var totalEntitiesCount = await _repository.WeatherConditions.CountAsync(expression);
+            var viewModels = _mapper.Map<IEnumerable<WeatherCondition>, IEnumerable<WeatherViewDto>>(entities);
+            
+            var listDto = new WeatherListDto()
+            {
+                CurrentPage = request.Dto.Page,
+                ItemsPerPage = request.Dto.ItemsNumber,
+                ListSorting = request.Dto.SortDir,
+                TotalPages = (int) Math.Ceiling((double) totalEntitiesCount / request.Dto.ItemsNumber),
                 TotalItems = totalEntitiesCount,
-                Entities = viewModels
+                Entities = viewModels,
+                Filters = request.Dto.Filters,
+                AvailableMonths = await _infoService.GetAvailableMonths(),
+                AvailableYears = await _infoService.GetAvailableYears()
             };
             
             _logger.LogInformation("{count} of weather conditions entities were recieved", entities.Count);
-            return new CommandResult<ListDto<WeatherViewDto>>(listDto);
+            return new CommandResult<WeatherListDto>(listDto);
         }
         catch (Exception exc)
         {
             _logger.LogError("Couldn't receive entities of type '{type}':\n{message}\n{stackTrace}",
                 typeof(WeatherCondition), exc.Message, exc.StackTrace);
             
-            return new CommandResult<ListDto<WeatherViewDto>>(isError: true);
+            return new CommandResult<WeatherListDto>(isError: true);
         }
     }
 }
--- src/Weather/Weather.Web/Services/Commands/Weather/GetWeatherConditionsRequest.cs
+++ src/Weather/Weather.Web/Services/Commands/Weather/GetWeatherConditionsRequest.cs
@@ -4,7 +4,7 @@ using Weather.Web.Models.Weather;
 
 namespace Weather.Web.Services.Commands.Weather;
 
-public class GetWeatherConditionsRequest : IRequest<CommandResult<ListDto<WeatherViewDto>>>
+public class GetWeatherConditionsRequest : IRequest<CommandResult<WeatherListDto>>
 {
     public GetEntitiesDto Dto { get; }
 
