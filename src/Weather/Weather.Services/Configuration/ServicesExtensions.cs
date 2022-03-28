85e34f5cd7301e38862d3a2888881b87591fedac
Elizaveta Orlova <orlovaev11@gmail.com>
2022-03-28 13:14:56 +0300

    fix ServicesExtensions.cs

--- src/Weather/Weather.Services/Configuration/ServicesExtensions.cs
+++ src/Weather/Weather.Services/Configuration/ServicesExtensions.cs
@@ -9,7 +9,7 @@ public static class ServicesExtensions
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
         services
-            .AddScoped<IInfoService, InfoService>()
+            .AddTransient<IInfoService, InfoService>()
             .AddSingleton(typeof(IFileService<>), typeof(ExcelService<>));
 
         return services;
